56d58a513cffaa87a3a12b505e1247746af01443
Hermanio09 <108497671+Hermanio09@users.noreply.github.com>
2022-10-07 15:41:19 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Csharp.Models;
+using System.Globalization;
+
+
+Pessoa p1 = new Pessoa(nome: "Buta", sobrenome: "Lima");
+Pessoa p2 = new Pessoa(nome: "Buta", sobrenome: "Guerra");
+
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
